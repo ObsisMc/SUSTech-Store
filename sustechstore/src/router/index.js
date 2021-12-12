8ad9bbb0b929714f6f5fd566dbe4be49f70b26bd
zrh_local <zrh735847540@outlook.com>
2021-12-12 10:18:25 +0800

    add history

--- sustechstore/src/router/index.js
+++ sustechstore/src/router/index.js
@@ -20,17 +20,104 @@ import errandmainpage from "../view/errandmainpage";
 import errandtaskpage from "../view/errandtaskpage";
 import errandsubmit from "../components/orderconfirm/errandsubmit";
 import errandpayment from "../components/orderconfirm/errandpayment";
+import axios from "axios";
 
 Vue.use(Router)
 
-export default new Router({
-  mode: "history",
-  routes: [
-    {
-      path: '/',
-      name: 'login',
-      component: loginpage
-    },
+// export default new Router({
+//   mode: "history",
+//   routes: [
+//     {
+//       path: '/',
+//       name: 'login',
+//       component: loginpage
+//     },
+//     {
+//       path: '/register',
+//       name: 'register',
+//       component: register
+//     },
+//     {
+//       path: '/main',
+//       name: 'main',
+//       component: homepage
+//     },
+//     {
+//       path: "/errand",
+//       name: "errandmain",
+//       component: errandmainpage
+//     },
+//     {
+//       path: '/search',
+//       name: 'search2',
+//       component: searchpage2
+//     },
+//     {
+//       path: '/test',
+//       name: 'test',
+//       component: test
+//     },
+//     {
+//       path: '/selfpage',
+//       name: 'selfpage',
+//       component: selfpage
+//     },
+//     {
+//       path: '/otherpage',
+//       name: 'otherpage',
+//       component: otherpage
+//     },
+//     {
+//       path: '/shoppingcart',
+//       name: 'shoppingcart',
+//       component: shoppingcartpage
+//     },
+//     {
+//       path: '/errandtask',
+//       name: 'errandtask',
+//       component: errandtaskpage
+//     },
+//     {
+//       path: '/product',
+//       name: 'product',
+//       component: product
+//     },
+//     {
+//       path: '/checkout/:id',
+//       name: 'checkoutpage',
+//       component: checkoutpage,
+//       children: [
+//         {path: '0', component: submitorder},
+//         {path: '1', component: payment},
+//         {path: '2', component: payresult},
+//         {path: '3', component: errandsubmit},
+//         {path: '4', component: errandpayment}
+//       ]
+//     },
+//     {
+//       path: '/payresult',
+//       name: 'success',
+//       component: payresult
+//     },
+//     {
+//       path: '/selfinfo',
+//       component: selfinfo
+//     },
+//     {
+//       path: '/*',
+//       name: 'notfound',
+//       component: notfound
+//     }
+//   ]
+// })
+
+const router = new Router({
+  mode:'history',
+  routes:[{
+    path: '/',
+    name: 'login',
+    component: loginpage
+  },
     {
       path: '/register',
       name: 'register',
@@ -106,6 +193,28 @@ export default new Router({
       path: '/*',
       name: 'notfound',
       component: notfound
-    }
-  ]
+    }]
 })
+
+router.beforeEach((to,form,next)=>{
+  const nextRoute = ['errandtask']
+  let isLogin = sessionStorage.getItem("token");
+  if (nextRoute.indexOf(to.name) >= 0) {
+    if (!isLogin) {
+      if (from.name === 'login') {
+        next('/')
+        return
+      }
+    }
+  }
+  if (to.name === 'login'){
+    if (isLogin){
+      next('/main')
+      return
+    }
+  }
+  next()
+})
+export default router
+
+
